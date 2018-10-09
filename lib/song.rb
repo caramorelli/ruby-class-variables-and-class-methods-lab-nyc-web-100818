1997566b9e69f94ce221ee76e3eb124732945a36
Cara Morelli <caramorelli7@gmail.com>
2018-10-09 21:42:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
   @@genres = []
   
   def initialize(name, artist, genre)
-    @name = []
-    @artist = []
-    @genre = []
+    @name = name
+    @artist = artist
+    @genre = genre
   end 
   
   def name 
