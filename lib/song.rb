a408dc42fe3a38c82fc1c5c337f7c95bdad49f48
Cara Morelli <caramorelli7@gmail.com>
2018-10-09 21:48:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,14 @@
 class Song 
   attr_accessor :name, :artist, :count, :genre
   
+  @@count == 0
+  
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end 
   
   def self.count 
